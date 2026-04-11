f6dacf1f76248aecc15d58f2e91bf2538ff3851c
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-11 09:17:15 +0530

    feat : add a new python library

--- Week_2/LinearRegression_ML.py
+++ Week_2/LinearRegression_ML.py
@@ -3,3 +3,12 @@ import pandas as pd
 df = pd.read_csv('hiring_salaries.csv')
 print(df.info())
 print(df.isnull().sum())
+print(df.describe())
+print(df.head())
+
+df.experience = df.experience.fillna('zero')
+print(df)
+
+df = df.fillna(df.median(numeric_only=True))
+print(df)
+print(df.isnull().sum(axis = 0))
