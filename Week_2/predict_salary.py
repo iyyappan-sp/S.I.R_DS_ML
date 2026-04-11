41a063839b8323c0bcde47124a655bd4539a0fab
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-11 10:50:56 +0530

    feat : predicted salary get user input

--- Week_2/predict_salary.py
+++ Week_2/predict_salary.py
@@ -1,8 +1,15 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import joblib
 salary = joblib.load('salary_model.pkl')
+for i in range(3):
 
-predict_val = salary.predict([[2,9,6]])    # experience,test_score_10,interview_score_10
-print(predict_val)
+    print('Enter the values for salary prediction')
+    experience = int(input("Enter your experience:"))
+    test_score = int(input("Enter your test score:"))
+    interview_score = int(input("Enter your interview score:"))
 
-predict_val = salary.predict([[12,10,10]])
-print(predict_val)
+    # predict using the model
+    predict_val = salary.predict([[experience, test_score, interview_score]])
+    print("Predicted salary:",predict_val[0])
