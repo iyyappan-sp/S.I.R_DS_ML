d723e249d517ef64c2d99f218f59ca2a15c40019
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-12 16:03:11 +0530

    feat : add get input from the user. using looping concepts

--- Week_2/predict_home_price.py
+++ Week_2/predict_home_price.py
@@ -12,12 +12,13 @@ print(predict_val)
 """
 
 # try it for input from the user
+#looping concepts
+for i in range(2):
+    print('Enter the values for home price prediction')
+    area = int(input("Enter your area size sq.ft:"))
+    bedrooms = int(input("Enter your bedrooms:"))
+    age = int(input("Enter your age:"))
 
-print('Enter the values for home price prediction')
-area = int(input("Enter your area size sq.ft:"))
-bedrooms = int(input("Enter your bedrooms:"))
-age = int(input("Enter your age:"))
-
-# predict using the model
-predict_val = home_price.predict([[area, bedrooms, age]])
-print("Predicted home price:",predict_val[0])
+    # predict using the model
+    predict_val = home_price.predict([[area, bedrooms, age]])
+    print("Predicted home price:",predict_val[0])
