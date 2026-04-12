8d686877e472e75aaf37d6b2c07047ba1faa403e
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-12 16:00:36 +0530

    feat : add get input from the user

--- Week_2/predict_home_price.py
+++ Week_2/predict_home_price.py
@@ -3,9 +3,21 @@ warnings.filterwarnings('ignore')
 
 import joblib
 home_price = joblib.load('home_price_model.pkl')
-
+"""
 predict_val = home_price.predict([[3000,3,40]])
 print(predict_val)
 
 predict_val = home_price.predict([[2500,4,5]])
 print(predict_val)
+"""
+
+# try it for input from the user
+
+print('Enter the values for home price prediction')
+area = int(input("Enter your area size sq.ft:"))
+bedrooms = int(input("Enter your bedrooms:"))
+age = int(input("Enter your age:"))
+
+# predict using the model
+predict_val = home_price.predict([[area, bedrooms, age]])
+print("Predicted home price:",predict_val[0])
