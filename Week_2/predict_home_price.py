dcd2c8fe537163f8abbd520efacd0a4f2631e9ff
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-12 15:50:25 +0530

    feat : add testing the model

--- Week_2/predict_home_price.py
+++ Week_2/predict_home_price.py
@@ -3,3 +3,9 @@ warnings.filterwarnings('ignore')
 
 import joblib
 home_price = joblib.load('home_price_model.pkl')
+
+predict_val = home_price.predict([[3000,3,40]])
+print(predict_val)
+
+predict_val = home_price.predict([[2500,4,5]])
+print(predict_val)
