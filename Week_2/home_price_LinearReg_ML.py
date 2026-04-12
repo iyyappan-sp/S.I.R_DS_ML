8f5af1b6d35d35e620ad0112be4fd17e0f8f28cd
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-12 15:13:58 +0530

    feat : add a new file

---
+++ Week_2/home_price_LinearReg_ML.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('home_prices.csv')
+print(df.info)
+print(df.describe())
+print(df.isnull().sum())
+
+df.bedrooms = df.bedrooms.fillna(df.bedrooms.median())
+print(df)
+
+from sklearn.linear_model import LinearRegression
+
+X = df.drop('price', axis=1)
+Y = df['price']
+
