dc9b57f9153d7ba65efa1d5e53592e02662ad96e
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-13 22:20:42 +0530

    feat : add Linear Regression 2

---
+++ Supervised_Learning/LinearReg_ML_2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("Fish.csv")
+data = df.copy()    # copy of data
+print(df.head())
+print(df.sample(5))    # want to see random use this
+print(df.shape)
+
+df.rename(columns = {'Length1':'Vertical','Length2':'Diagonal','Length3':'Cross'},inplace = True)
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
