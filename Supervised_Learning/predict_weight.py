3666a89ac0efeb5b384fa7f43239f022913b52ab
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-14 15:11:53 +0530

    feat : add use loop concepts predict the value from user input

--- Supervised_Learning/predict_weight.py
+++ Supervised_Learning/predict_weight.py
@@ -1,12 +1,14 @@
+import warnings
+warnings.filterwarnings('ignore')
 import joblib
 fish_weight = joblib.load('weight_predict.pkl')
+for i in range(2):
+    print("Enter the value for fish weight predeiction")
+    Vertical = int(input("Enter your vertical value:"))
+    Diagonal = int(input("Enter your diagonal value:"))
+    Cross = int(input("Enter your cross value:"))
+    Height = int(input("Enter your height value:"))
+    Width = int(input("Enter your width value:"))
 
-print("Enter the value for fish weight predeiction")
-Vertical = int(input("Enter your vertical value:"))
-Diagonal = int(input("Enter your diagonal value:"))
-Cross = int(input("Enter your cross value:"))
-Height = int(input("Enter your height value:"))
-Width = int(input("Enter your width value:"))
-
-prediction = fish_weight.predict([[Vertical, Diagonal, Cross, Height, Width]])
-print(prediction)
+    prediction = fish_weight.predict([[Vertical, Diagonal, Cross, Height, Width]])
+    print(prediction)
