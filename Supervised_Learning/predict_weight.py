e7dcaf36d5bba01a780599d533e07deb087f3f64
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-14 15:08:55 +0530

    feat : add predict the value from user input

---
+++ Supervised_Learning/predict_weight.py
@@ -0,0 +1,12 @@
+import joblib
+fish_weight = joblib.load('weight_predict.pkl')
+
+print("Enter the value for fish weight predeiction")
+Vertical = int(input("Enter your vertical value:"))
+Diagonal = int(input("Enter your diagonal value:"))
+Cross = int(input("Enter your cross value:"))
+Height = int(input("Enter your height value:"))
+Width = int(input("Enter your width value:"))
+
+prediction = fish_weight.predict([[Vertical, Diagonal, Cross, Height, Width]])
+print(prediction)
