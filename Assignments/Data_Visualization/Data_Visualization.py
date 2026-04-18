c8173286d8309b44b065f529b8a0b1c83cb84d20
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-18 12:34:25 +0530

    feat : add Scatter plot

---
+++ Assignments/Data_Visualization/Data_Visualization.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('tips.csv')    # reading the dataset
+
+print(data.head())    # printing the top 5 rows
+
+# scatter plot 1 with day against trip
+plt.scatter(data['day'], data['tip'], c=data['size'], s=data['total_bill'])
+plt.title('Scatter Plot 1')    # add title for plot
+plt.xlabel('Day')
+plt.ylabel('Tip')
+plt.colorbar()
+plt.show()
