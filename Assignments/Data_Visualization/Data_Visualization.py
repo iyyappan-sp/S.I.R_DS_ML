16b22d3ec7a11d7949f837db45e98a008736815f
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-18 18:03:07 +0530

    feat : add lineplot using seaborn

--- Assignments/Data_Visualization/Data_Visualization.py
+++ Assignments/Data_Visualization/Data_Visualization.py
@@ -6,7 +6,7 @@ data = pd.read_csv('tips.csv')    # reading the dataset
 
 print(data.head())    # printing the top 5 rows
 
-# scatter plot 1 with day against trip
+# scatter plot 1 with day against trip---matplotlib
 plt.scatter(data['day'], data['tip'], c=data['size'], s=data['total_bill'])
 plt.title('Scatter Plot 1')    # add title for plot
 plt.xlabel('Day')
@@ -14,7 +14,7 @@ plt.ylabel('Tip')
 plt.colorbar()
 plt.show()
 
-# scatter plot 2
+# scatter plot 2---matplotlib
 plt.plot(data['tip'])
 plt.plot(data['size'])
 plt.title('Scatter Plot 2')
@@ -22,14 +22,20 @@ plt.xlabel('Day')
 plt.ylabel('Tip')
 plt.show()
 
-# bar chart with day against trip
+# bar chart with day against trip---matplotlib
 plt.bar(data['day'], data['tip'])
 plt.title("Bar Chart")
 plt.xlabel('Day')
 plt.ylabel('Tip')
 plt.show()
 
-# histogram of total bills
+# histogram of total bills-matplotlib
 plt.hist(data['total_bill'])
 plt.title('Histogram')
 plt.show()
+
+# draw lineplot---seaborn
+sns.lineplot(x="sex", y="total_bill", data=data)
+plt.title('Title using Matplotlib Function')    # sns---seaborn , plt---matplotlib
+plt.show()
+
