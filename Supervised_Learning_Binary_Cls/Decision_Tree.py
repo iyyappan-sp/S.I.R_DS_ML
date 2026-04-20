7ecdf1163f66c9930d877abf439802d77712ea93
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-20 21:42:47 +0530

    feat : add train_test_split

--- Supervised_Learning_Binary_Cls/Decision_Tree.py
+++ Supervised_Learning_Binary_Cls/Decision_Tree.py
@@ -6,3 +6,13 @@ print(dataset.info())
 print(dataset.describe())
 counts = dataset['Outcome'].value_counts()
 print(counts)
+
+X = dataset.iloc[:, 0:7]
+y = dataset.iloc[:,8]
+print(X)
+print(y)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2, random_state=42)
+diabetes_counts = y_train.value_counts()
+print(diabetes_counts)
