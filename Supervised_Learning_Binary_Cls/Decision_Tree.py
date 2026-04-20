4a5754c78d0c8fd97d46769db17cb53cefda8169
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-20 21:45:14 +0530

    feat : add model fit and predict

--- Supervised_Learning_Binary_Cls/Decision_Tree.py
+++ Supervised_Learning_Binary_Cls/Decision_Tree.py
@@ -16,3 +16,9 @@ from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2, random_state=42)
 diabetes_counts = y_train.value_counts()
 print(diabetes_counts)
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+print(y_pred)
