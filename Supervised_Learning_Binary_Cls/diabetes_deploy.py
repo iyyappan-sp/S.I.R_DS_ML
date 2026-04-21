a65804ed231fd4f45b7a41f50c85626b11cb4a02
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-21 11:14:38 +0530

    feat : add diabetes deployment input from user

--- Supervised_Learning_Binary_Cls/diabetes_deploy.py
+++ Supervised_Learning_Binary_Cls/diabetes_deploy.py
@@ -1,10 +1,33 @@
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 import joblib
 model = joblib.load('diabetes_rf.pkl')
 
-import pandas as pd
+print('Enter the values of diagnostic measurements')
+Pregnancies = float(input("Enter the patient's Pregnancies values: "))
+Glucose = float(input("Enter the patient's Glucose values: "))
+BloodPressure = float(input("Enter the patient's BloodPressure values: "))
+SkinThickness = float(input("Enter the patient's SkinThickness values: "))
+Insulin = float(input("Enter the patient's Insulin values: "))
+BMI = float(input("Enter the patient's BMI values: "))
+DiabetesPedigreeFunction = float(input("Enter the patient's DiabetesPedigreeFunction values: "))
+Age = int(input("Enter the patient's Age values: "))
+
+input_data = [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]]
+predict_val = model.predict(input_data)
+
+if predict_val[0] == 1:
+    result = 'Positive'
+else:
+    result = 'Negative'
+print(f"\nPrediction Result: {result}")
+
+"""
 newdata = pd.read_csv('newdataset.csv')
 print(newdata.info())
 print(newdata)
+
 
 predict = model.predict(newdata)
 print(predict)
@@ -12,3 +35,4 @@ newdata['Outcome'] = predict
 print(newdata)
 newdata.loc[newdata['Outcome'] == 1, 'Outcome'] = 'Positive'
 print(newdata)
+"""
