296893619b2826fcd4c9cc98150f8c278e77d9e5
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-04-27 14:06:18 +0530

    feat : added preprocessing the data

--- Project/tn_govt_scheme_recom_sys.py
+++ Project/tn_govt_scheme_recom_sys.py
@@ -42,3 +42,42 @@ citizens = pd.read_csv("tn_citizen_dataset.csv")
 schemes  = pd.read_csv("tn_schemes.csv")
 citizens.info()
 schemes.info()
+
+# step:3: ---- Preprocessing Data
+# these the columns are unnecessary
+citizens = citizens.drop(columns=['name', 'ration_card_type', 'bank_account', 'land_ownership', 'land_holding_acres'])
+# fill missing values
+citizens['religion'] = citizens['religion'].fillna('Unknown')
+citizens['education'] = citizens['education'].fillna(citizens['education'].mode()[0])
+citizens['annual_income'] = citizens['annual_income'].fillna(citizens['annual_income'].median())
+citizens['marital_status'] = citizens['marital_status'].fillna(citizens['marital_status'].mode()[0])
+citizens['district'] = citizens['district'].fillna(citizens['district'].mode()[0])
+citizens['family_size'] = citizens['family_size'].fillna(citizens['family_size'].median())
+citizens['disability_status'] = citizens['disability_status'].fillna('No')
+
+# fill missing values on education column and use mapping (higher education → higher number)
+
+edu_levels = {
+    'No Formal Education': 0, '5th Pass': 1, '8th Pass': 2,
+    '10th Pass': 3, '12th Pass': 4, 'Graduate': 5, 'Post Graduate': 6
+}
+citizens['edu_rank'] = citizens['education'].map(edu_levels).fillna(0).astype(int)
+
+# Some schemes cover coastal districts, others target major urban areas
+coastal_districts = {'Chennai', 'Tiruvallur', 'Chengalpattu', 'Villupuram',
+                     'Cuddalore', 'Mayiladuthurai', 'Nagapattinam', 'Tiruvarur', 'Thanjavur',
+                     'Pudukkottai', 'Ramanathapuram', 'Thoothukudi', 'Tirunelveli', 'Kanyakumari'}
+
+urban_districts = {'Chennai', 'Chengalpattu', 'Tiruvallur', 'Coimbatore', 'Tiruppur',
+                   'Madurai', 'Salem', 'Tiruchirappalli', 'Erode', 'Kancheepuram'}
+
+# to categorized the districts
+def get_area_type(district):
+    if district in coastal_districts:
+        return 'Coastal'
+    elif district in urban_districts:
+        return 'Urban'
+    return 'Rural'
+
+citizens['district_type'] = citizens['district'].apply(get_area_type)
+citizens.info()
