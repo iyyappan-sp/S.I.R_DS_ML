968b9f1ba7191c91c540f6c1c92492922d4f717d
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-05-01 20:42:22 +0530

    feat : added stratify to balanced data

--- Supervised_Learning_Multiclass/Iris_prediction.py
+++ Supervised_Learning_Multiclass/Iris_prediction.py
@@ -16,6 +16,8 @@ print(X.head())
 print(y.head())
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-print(X_train.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42, stratify = y)
 print(y_train.shape)
+print(y_train.value_counts())
+print(y_test.shape)
+print(y_test.value_counts())
