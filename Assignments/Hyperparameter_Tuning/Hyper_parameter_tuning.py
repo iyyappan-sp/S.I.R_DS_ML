0f47c4599262149e583a51c1c4f10d73f3c85ecd
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-05-02 08:24:17 +0530

    feat : added svm model

--- Assignments/Hyperparameter_Tuning/Hyper_parameter_tuning.py
+++ Assignments/Hyperparameter_Tuning/Hyper_parameter_tuning.py
@@ -6,3 +6,10 @@ df['flower'] = iris_dataset.target
 df['flower'] = df['flower'].apply(lambda x: iris_dataset.target_names[x])
 print(df)
 print(df.columns)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset.data, iris_dataset.target, test_size = 0.3)
+
+model = svm.SVC(kernel='rbf', C=30, gamma='auto')
+model.fit(X_train, y_train)
+model.score(X_test, y_test)
