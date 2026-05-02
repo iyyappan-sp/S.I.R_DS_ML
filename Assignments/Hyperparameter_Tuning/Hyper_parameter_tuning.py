8a7417005b96f5a0837b064edc68bd77afd98a7f
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-05-02 09:40:21 +0530

    feat : added choosing best model

--- Assignments/Hyperparameter_Tuning/Hyper_parameter_tuning.py
+++ Assignments/Hyperparameter_Tuning/Hyper_parameter_tuning.py
@@ -64,3 +64,43 @@ rs = RandomizedSearchCV(svm.SVC(gamma='auto'), {
 rs.fit(iris_dataset.data, iris_dataset.target)
 rs_result_score = pd.DataFrame(rs.cv_results_)[['param_C', 'param_kernel', 'mean_test_score']]
 print(rs_result_score)
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+model_params = {
+    'svm': {
+        'model': svm.SVC(gamma='auto'),
+        'params' : {
+            'C': [1,10,20],
+            'kernel': ['rbf','linear']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params' : {
+            'n_estimators': [1,5,10]
+        }
+    },
+    'logistic_regression' : {
+        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
+        'params': {
+            'C': [1,5,10]
+        }
+    }
+}
+
+scores = []
+
+for model_name, mp in model_params.items():
+    clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False)
+    clf.fit(iris_dataset.data, iris_dataset.target)
+    scores.append({
+        'model': model_name,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+
+df = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+print(df)
