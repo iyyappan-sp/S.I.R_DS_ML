2af155dd452506afea676b0c4696e7980b6647df
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-05-07 20:58:54 +0530

    feat : added wrapper method(RFE)

--- Assignments/Feature_Selection/Feature_Selection.py
+++ Assignments/Feature_Selection/Feature_Selection.py
@@ -36,3 +36,21 @@ plt.bar(x.columns, k_best.scores_)
 plt.xticks(rotation=45)
 plt.title("Chi-Square Feature Scores")
 plt.show()
+
+# Wrapper Method (RFE)
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression(max_iter=1000)
+
+rfe = RFE(estimator=model, n_features_to_select=3)
+fit = rfe.fit(x, y)
+
+print("Number of features")
+print(fit.n_features_)
+
+print("Selected features")
+print(fit.support_)
+
+print("Feature ranking")
+print(fit.ranking_)
