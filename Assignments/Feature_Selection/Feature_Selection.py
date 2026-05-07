3b7bfaca3ebca82cf3a69f7c3431e785702f6c6a
iyyappan-sp <sireddy158iyappan@gmail.com>
2026-05-07 20:57:06 +0530

    feat : added filter methode(chi2)

--- Assignments/Feature_Selection/Feature_Selection.py
+++ Assignments/Feature_Selection/Feature_Selection.py
@@ -15,3 +15,17 @@ print(data.columns)
 x = data.iloc[:, 0:8]
 y = data.iloc[:, 8]
 
+# Filter Method (Chi-Square)
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+
+chi2_best = SelectKBest(score_func=chi2, k=4)
+k_best = chi2_best.fit(x, y)
+
+print("Chi-Square Scores")
+print(k_best.scores_)
+
+k_features = k_best.transform(x)
+
+print("Selected features first 5 rows")
+print(k_features[0:5, :])
